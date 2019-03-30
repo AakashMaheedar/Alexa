39b56b650d91f0297cbfa76adc729dd0d2d0b0ac
Aakash <45240744+AakashMaheedar@users.noreply.github.com>
2019-03-30 11:23:24 +0530

    version 1

---
+++ Alexa/ModulePyFiles/firefox.py
@@ -0,0 +1,27 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+print("Welecome to Voice Command Protal How can i help You:")
+def openchrome():
+	driver=webdriver.Firefox(executable_path="C:\Alexa\Exe\geckodriver.exe")
+
+	while True:
+		do=input("Enter command:")
+			
+		if do=='search':
+			searchlink=input("Enter any link :")
+			driver.get("https://"+searchlink)
+		elif do=='scrolldown':
+			driver.execute_script("window.scrollBy(0,500)","")
+		elif do=='Maximize window':
+			driver.maximize_window()
+		elif do=='close':
+			driver.close()
+		elif do=='open':
+			openchrome()
+		elif do=='quit':
+			driver.quit()
+			break
+
+openchrome()
